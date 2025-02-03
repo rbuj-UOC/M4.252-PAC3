eb3a16c8d05af131b786cf63e55cf485bd87546e
Robert Buj <robert.buj@gmail.com>
2025-02-03 12:14:11 +0100

    eslint-plugin-html

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import html from "eslint-plugin-html";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {files: ["**/*.html"], plugins: { html } },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   eslintConfigPrettier,
